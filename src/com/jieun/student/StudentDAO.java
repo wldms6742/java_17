239eb1d893078df6e95b0a3bff1a3a5935ab1d07
wldms6742 <36650354+wldms6742@users.noreply.github.com>
2018-03-15 15:14:10 +0900

    ad

--- src/com/jieun/student/StudentDAO.java
+++ src/com/jieun/student/StudentDAO.java
@@ -6,10 +6,10 @@ import java.sql.PreparedStatement;
 import com.jieun.util.DBConnector;
 
 public class StudentDAO {
-	public void insert() throws Exception {
+	public void insert(StudentDTO studentDTO) throws Exception {
 		Connection con = DBConnector.getConnect();
 		String sql = "insert into student values(?,?,?,?,?,?)";
 		PreparedStatement st = con.prepareStatement(sql);
-		
+		st.setInt(1, studentDTO.getId());
 	}
 }
